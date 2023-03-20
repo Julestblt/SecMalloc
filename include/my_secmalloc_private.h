c9475ce12488917bc9f374d037e85e5ac92583d2
JULES <jules.thibault@ecole2600.com>
2023-03-20 22:17:06 +0100

    feat: add memory struct and canary

--- include/my_secmalloc_private.h
+++ include/my_secmalloc_private.h
@@ -5,4 +5,19 @@
  * Ici vous pourrez faire toutes les déclarations de variables/fonctions pour votre usage interne
  * */
 
+// Structure qui va contenir les informations sur la mémoire allouée.
+typedef struct s_block
+{
+    size_t size;
+    struct s_block *next;
+    int free;
+    void *ptr;
+    char data[1];
+    unsigned long canary;
+} t_block;
+
+#define BLOCK_SIZE (sizeof(t_block))
+
+#define CANARY_SIZE (sizeof(unsigned long))
+
 #endif
--- src/my_secmalloc.c
+++ src/my_secmalloc.c
@@ -4,25 +4,115 @@
 #include "my_secmalloc.h"
 #include "my_secmalloc_private.h"
 
-void *my_malloc(size_t size)
+t_block *g_base = NULL;
+
+// Fonction qui permet de générer un canary aléatoire.
+CANARY_SIZE generate_canary()
 {
-    // On vérifie que la taille demandée n'est pas 0, si c'est le cas on retourne NULL.
-    if (size == 0)
+    return (rand() % 1000000);
+}
+
+// Fonction qui permet de trouver un bloc libre de la taille demandée.
+t_block *find_free_block(t_block **last, size_t size)
+{
+    // On parcours la liste chainée jusqu'à trouver un bloc libre de la taille demandée.
+    t_block *current = g_base;
+    // On stocke le dernier bloc pour pouvoir le lier au nouveau bloc.
+    while (current && !(current->free && current->size >= (size + CANARY_SIZE)))
+    {
+        // On stocke le dernier bloc pour pouvoir le lier au nouveau bloc.
+        *last = current;
+        // On passe au bloc suivant.
+        current = current->next;
+    }
+    // On retourne le bloc libre trouvé ou NULL si aucun bloc libre n'a été trouvé.
+    return (current);
+}
+
+// Fonction qui permet d'allouer de la mémoire.
+t_block *request_space(t_block *last, size_t size)
+{
+    // On alloue de la mémoire pour le nouveau bloc.
+    t_block *new_block;
+    new_block = mmap(0, size + BLOCK_SIZE, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+
+    // On vérifie que l'allocation s'est bien passée.
+    if (new_block == MAP_FAILED)
     {
         return (NULL);
     }
 
-    // On utilise mmap pour allouer de la mémoire.
-    void *ptr = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+    // On initialise le nouveau bloc.
+    new_block->size = size;
+    new_block->next = NULL;
+    new_block->free = 0;
+    new_block->ptr = new_block->data;
+    new_block->canary = generate_canary();
 
-    // On vérifie que mmap n'a pas retourné d'erreur, si c'est le cas on retourne NULL.
-    if (ptr == MAP_FAILED)
+    // On lie le nouveau bloc au dernier bloc.
+    if (last)
     {
-        return (NULL);
+        last->next = new_block;
+    }
+
+    // On retourne le nouveau bloc.
+    return (new_block);
+}
+
+void *my_malloc(size_t size)
+{
+    // On vérifie que la taille demandée est valide.
+    if (size <= 0)
+    {
+        return NULL;
+    }
+
+    // On vérifie si la liste chainée est vide.
+    if (!g_base)
+    {
+        // On alloue de la mémoire pour le nouveau bloc.
+        t_block *new_block = request_space(NULL, size);
+
+        // On vérifie que l'allocation s'est bien passée, sinon on retourne NULL.
+        if (!new_block)
+        {
+            return (NULL);
+        }
+
+        // On initialise la liste chainée.
+        g_base = new_block;
+    }
+    // Si la liste chainée n'est pas vide.
+    else
+    {
+        // On cherche un bloc libre de la taille demandée.
+        t_block *last_block = g_base;
+
+        // On stocke le dernier bloc pour pouvoir le lier au nouveau bloc.
+        t_block *free_block = find_free_block(&last_block, size);
+
+        // On vérifie si un bloc libre a été trouvé.
+        if (free_block)
+        {
+            // Si oui, on defini le bloc comme occupé.
+            free_block->free = 0;
+        }
+        // Si aucun bloc libre n'a été trouvé.
+        else
+        {
+            // On alloue de la mémoire pour le nouveau bloc.
+            free_block = request_space(last_block, size);
+
+            // On vérifie que l'allocation s'est bien passée, sinon on retourne NULL.
+            if (!free_block)
+            {
+                return NULL;
+            }
+        }
     }
 
     // On retourne l'adresse de la mémoire allouée.
-    return (ptr);
+    return (free_block->ptr);
 }
 
 void my_free(void *ptr)
