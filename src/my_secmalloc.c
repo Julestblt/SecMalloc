92cd299339cf58a509b0857f909ce856f2869cb3
Jules THIBAULT <70810590+Julestblt@users.noreply.github.com>
2023-03-29 11:14:59 +0200

    feat: malloc (#6)
    * feat: basic malloc() implementation
    
    * feat: add memory struct and canary
    
    * fix: compilation error

--- src/my_secmalloc.c
+++ src/my_secmalloc.c
@@ -4,30 +4,137 @@
 #include "my_secmalloc.h"
 #include "my_secmalloc_private.h"
 
-void    *my_malloc(size_t size)
+t_block *g_base = NULL;
+
+// Fonction qui permet de générer un canary aléatoire.
+unsigned long generate_canary()
 {
-    (void) size;
+    return (rand() % 1000000);
+}
+
+// Fonction qui permet de trouver un bloc libre de la taille demandée.
+t_block *find_free_block(t_block **last, size_t size)
+{
+    // On parcours la liste chainée jusqu'à trouver un bloc libre de la taille demandée.
+    t_block *current = g_base;
+    // On stocke le dernier bloc pour pouvoir le lier au nouveau bloc.
+    while (current && !(current->free && current->size >= (size + sizeof(unsigned long))))
+    {
+        // On stocke le dernier bloc pour pouvoir le lier au nouveau bloc.
+        *last = current;
+        // On passe au bloc suivant.
+        current = current->next;
+    }
+    // On retourne le bloc libre trouvé ou NULL si aucun bloc libre n'a été trouvé.
+    return (current);
+}
+
+// Fonction qui permet d'allouer de la mémoire.
+t_block *request_space(t_block *last, size_t size)
+{
+    // On alloue de la mémoire pour le nouveau bloc.
+    t_block *new_block;
+    new_block = mmap(0, size + BLOCK_SIZE, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+
+    // On vérifie que l'allocation s'est bien passée.
+    if (new_block == MAP_FAILED)
+    {
+        return (NULL);
+    }
+
+    // On initialise le nouveau bloc.
+    new_block->size = size;
+    new_block->next = NULL;
+    new_block->free = 0;
+    new_block->ptr = new_block->data;
+    new_block->canary = generate_canary();
+
+    // On lie le nouveau bloc au dernier bloc.
+    if (last)
+    {
+        last->next = new_block;
+    }
+
+    // On retourne le nouveau bloc.
+    return (new_block);
+}
+
+void *my_malloc(size_t size)
+{
+    // On vérifie que la taille demandée est valide.
+    if (size <= 0)
+    {
+        return (NULL);
+    }
+
+    // On vérifie si la liste chainée est vide.
+    if (!g_base)
+    {
+        // On alloue de la mémoire pour le nouveau bloc.
+        t_block *new_block = request_space(NULL, size);
+
+        // On vérifie que l'allocation s'est bien passée, sinon on retourne NULL.
+        if (!new_block)
+        {
+            return (NULL);
+        }
+
+        // On initialise la liste chainée.
+        g_base = new_block;
+    }
+    // Si la liste chainée n'est pas vide.
+    else
+    {
+        // On cherche un bloc libre de la taille demandée.
+        t_block *last_block = g_base;
+
+        // On stocke le dernier bloc pour pouvoir le lier au nouveau bloc.
+        t_block *free_block = find_free_block(&last_block, size);
+
+        // On vérifie si un bloc libre a été trouvé.
+        if (free_block)
+        {
+            // Si oui, on defini le bloc comme occupé.
+            free_block->free = 0;
+        }
+        // Si aucun bloc libre n'a été trouvé.
+        else
+        {
+            // On alloue de la mémoire pour le nouveau bloc.
+            free_block = request_space(last_block, size);
+
+            // On vérifie que l'allocation s'est bien passée, sinon on retourne NULL.
+            if (!free_block)
+            {
+                return (NULL);
+            }
+
+            // On retourne l'adresse de la mémoire allouée.
+            return (free_block->ptr);
+        }
+    }
+
+    // On retourne l'adresse de la mémoire allouée.
+    return (g_base->ptr);
+}
+
+void my_free(void *ptr)
+{
+    (void)ptr;
+}
+
+void *my_calloc(size_t nmemb, size_t size)
+{
+    (void)nmemb;
+    (void)size;
     return NULL;
 }
 
-void    my_free(void *ptr)
+void *my_realloc(void *ptr, size_t size)
 {
-    (void) ptr;
-}
-
-void    *my_calloc(size_t nmemb, size_t size)
-{
-    (void) nmemb;
-    (void) size;
+    (void)ptr;
+    (void)size;
     return NULL;
-}
-
-void    *my_realloc(void *ptr, size_t size)
-{
-    (void) ptr;
-    (void) size;
-    return NULL;
-
 }
 
 #ifdef DYNAMIC
@@ -35,20 +142,20 @@ void    *my_realloc(void *ptr, size_t size)
  * Lorsque la bibliothèque sera compilé en .so les symboles malloc/free/calloc/realloc seront visible
  * */
 
-void    *malloc(size_t size)
+void *malloc(size_t size)
 {
     return my_malloc(size);
 }
-void    free(void *ptr)
+void free(void *ptr)
 {
     my_free(ptr);
 }
-void    *calloc(size_t nmemb, size_t size)
+void *calloc(size_t nmemb, size_t size)
 {
     return my_calloc(nmemb, size);
 }
 
-void    *realloc(void *ptr, size_t size)
+void *realloc(void *ptr, size_t size)
 {
     return my_realloc(ptr, size);
 }
--- test/test.c
+++ test/test.c
@@ -5,14 +5,9 @@
 #include "my_secmalloc.h"
 #include "my_secmalloc_private.h"
 
-Test(mmap, simple) {
-    // Question: Est-ce que printf fait un malloc ?
-    printf("Ici on fait un test simple de mmap\n");
-    void *ptr = mmap(NULL, 4096, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
-    cr_expect(ptr != NULL);
-    int res = munmap(ptr, 4096);
-    cr_expect(res == 0);
-    char *str = my_malloc(15);
-    strncpy(str, "ca marche", 15);
-    printf("bla %s\n", str);
+// Test simple de malloc.
+Test(my_malloc, simple_test)
+{
+    char *str = malloc(15);
+    cr_expect_not_null(str);
 }
